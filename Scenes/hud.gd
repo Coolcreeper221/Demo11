d0bb6538e9bf133f44005726fb2b326bc310824e
Coolcreeper221 <113726609+Coolcreeper221@users.noreply.github.com>
2024-05-21 08:00:57 -0400

    Add files via upload

--- Scenes/hud.gd
+++ Scenes/hud.gd
@@ -2,8 +2,14 @@ extends Control
 
 
 # Called when the node enters the scene tree for the first time.
-#func _ready():
-	#for i in Global.items.size():
-	#	$CanvasLayer/Control/Items.get_child(i).get_child(0).frame = Global.items[i].sprite_index
+func _ready():
+	for i in Global.items.size():
+		$HUD/Control/Items.get_child(i).get_child(0).frame = Global.items[i].sprite_index
 func _process(_delta):
-	$CanvasLayer/Label.text = "  Bullets: " + str($"../Spawner".get_child_count())+"\n  FPS: "+str(Engine.get_frames_per_second())
+	$HUD/Label.text = "  Bullets: " + str($"../Spawner".get_child_count())+"\n  FPS: "+str(Engine.get_frames_per_second())+"\n  Ammo: " + str(Global.ammo)+"\n  Room: " + str(Global.clearedrooms.size()+1)
+	
+	$Death.visible =Global.player.health <= 0
+
+
+func _on_button_pressed():
+	get_tree().reload_current_scene()
--- Scenes/rooms/baseroom.gd
+++ Scenes/rooms/baseroom.gd
@@ -4,14 +4,36 @@ class_name room
 @export var exit:Position2D
 @export var aenter:float
 @export var aexit:float
+@export var prenemies:Array
+@onready var gate = get_node_or_null("exit/NinePatchRect/StaticBody2D/CollisionShape2D")
 func _ready():
 	connect("screen_entered",_show)
 	connect("screen_exited",_hide)
+	
 	$Baseroom.hide()
 	$Baseroom.add_to_group("navigation",true)
 	$".".rect = Rect2i($Baseroom.get_used_rect().position *16, $Baseroom.get_used_rect().size *16)
-
 	clear_excess()
+func _exit(body):
+
+	if !Global.clearedrooms.has(name) and body.name == "player":
+		$exit/NinePatchRect/StaticBody2D/CollisionShape2D.set_deferred("disabled",false)
+		$exit/NinePatchRect.visible = true
+		Global.clearedrooms.append(name)
+func _process(_delta):
+	if is_instance_valid(gate):
+		if $Baseroom.visible:
+			for i in prenemies:
+				if !has_node(i):
+					prenemies.erase(i)
+				if !prenemies:
+					$exit/NinePatchRect/StaticBody2D/CollisionShape2D.disabled = true
+					$room.connect("body_exited",_exit)
+					$exit/NinePatchRect.visible = false
+		else:
+			$exit/NinePatchRect/StaticBody2D/CollisionShape2D.disabled = false
+			$exit/NinePatchRect.visible = true
+	
 func _show():
 	$Baseroom.visible = true
 func _hide():
@@ -23,11 +45,13 @@ func clear_excess():
 	get_exit_angle()
 	get_entrance_angle()
 	for i in get_children():
-		if i.name == "entrance" or i.name == "exit":
+		if i.name == "entrance":
 			i.queue_free()
-		elif i.name == "melee":
+		elif i.name.contains("melee"):
 			await get_tree().process_frame
 			i.reparent(get_tree().get_root().get_child(1))
+			prenemies.append(i.get_path())
+
 	
 
 func get_exit():
