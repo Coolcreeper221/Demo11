8d796af80a048c952bece11feca813ec673774dd
Coolcreeper221 <113726609+Coolcreeper221@users.noreply.github.com>
2024-05-21 07:59:53 -0400

    Add files via upload

--- global.gd
+++ global.gd
@@ -3,8 +3,17 @@ extends Node
 @export var items:Array[item]
 @export var player:CharacterBody2D
 @export var shake:float
-
+@export var ammo:int = 10
+@export var ammo_timer:float
+@export var ammo_cooldown:float
+@export var clearedrooms:Array
 func _process(delta):
+	print(player.str_to_var("speed"))
+	ammo_timer += delta
+	if ammo_timer >= ammo_cooldown:
+		ammo_timer = 0 
+		ammo +=1 
+	shake = clamp(shake,0,0.05)
 	if shake:
 		var cam = player.find_child("Camera2D")
 		randomize()
